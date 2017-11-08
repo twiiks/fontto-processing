bdd2228dfa4728bfa6cfef46cedfec3369afde14
장상현 <jexjang92@gmail.com>
2017-11-08 20:19:28 +0900

    trim_resize_PIL 구현
    ---
    Redmine Issue Progress / ID / Time :  100% #107 @20m
    ---
    What :
    - demo-fontto-processing의 trim_resize_image/trim_resize_image 와 scale 가져오기
    
    Why :
    초기화
    
    ---
    *Signed-off-by: sjang42 <jexjang92@gmail.com>*

--- modify.py
+++ modify.py
@@ -1,9 +1,35 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
+from PIL import Image, ImageChops, ImageOps
+
+
+def scale(image, max_size, method=Image.ANTIALIAS):
+    im_aspect = float(image.size[0]) / float(image.size[1])
+    out_aspect = float(max_size[0]) / float(max_size[1])
+    if im_aspect >= out_aspect:
+        scaled = image.resize(
+            (max_size[0], int((float(max_size[0]) / im_aspect) + 0.5)), method)
+    else:
+        scaled = image.resize(
+            (int((float(max_size[1]) * im_aspect) + 0.5), max_size[1]), method)
+
+    offset = (int((max_size[0] - scaled.size[0]) / 2), int(
+        (max_size[1] - scaled.size[1]) / 2))
+    # print(offset)
+    back = Image.new("RGB", max_size, "white")
+    back.paste(scaled, offset)
+    return back
+
 
 def trim_resize_PIL(input_PIL, width, height, border):
-    modified_PIL = "trimmed, resized PIL"
-    return modified_PIL
+    bg = Image.new(input_PIL.mode, input_PIL.size, input_PIL.getpixel((0, 0)))
+    diff = ImageChops.difference(input_PIL, bg)
+    diff = ImageChops.add(diff, diff, 2.0, -100)
+    bbox = diff.getbbox()
+    image_output = input_PIL.crop(bbox)
+    image_output = scale(image_output, [width, height])
+    image_output = ImageOps.expand(image_output, border=border, fill='white')
+    return image_output
 
 
 def noise_filter(PIL_img):
@@ -19,4 +45,3 @@ def vectoralize(PIL_img):
 def svgs2ttf(svg_set):
     ttf_converted = "converted svg_set to ttf file"
     return ttf_converted
-    
