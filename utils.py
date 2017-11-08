4af2315ad6b0ddba961d895108f2e4bd55127053
장상현 <jexjang92@gmail.com>
2017-11-08 20:18:02 +0900

    url2img 구현
    ---
    Redmine Issue Progress / ID / Time :  100% #112 @20m
    
    ---
    What :
    - demo-fontto-processing의 utils/url2img 가져오기
    
    - import(urllib/PIL/io) 추가
    
    Why :
    초기화
    ---
    *Signed-off-by: sjang42 <jexjang92@gmail.com>*

--- utils.py
+++ utils.py
@@ -1,8 +1,28 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
+import urllib
+import urllib.request
+from PIL import Image
+from io import BytesIO
+
+
 def url2img(input_address):
-    input_PIL = "url -> pil"
+    """
+    - fontto_pix2pix에서 사용할 수 있도록 url을 통해 이미지를 로드하고 PIL Image로 변환하여 반환
+    - 입력 : url
+    - 반환 : image
+    """
+    # url -> bytes
+    url = urllib.parse.urlsplit(input_address)
+    url = list(url)
+    url[2] = urllib.parse.quote(url[2])
+    url = urllib.parse.urlunsplit(url)
+    url = urllib.request.urlopen(url).read()
+    bytesFromS3 = BytesIO(url)
+    # bytes -> image
+    input_PIL = Image.open(bytesFromS3)
+
     return input_PIL
 
 
@@ -14,4 +34,3 @@ def store2S3(userID, count, uni, PIL, env, s3key):
 def ttf2S3(userID, count, ttf_converted, env):
     ttf_address = "ttf address saved in S3"
     return ttf_address
-
