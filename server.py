831dd4dead933a1ee82023e6c92489c171b78f8d
sungjunyoung <wnsdud1861@gmail.com>
2017-12-03 00:45:19 +0900

    flask server added

---
+++ server.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from flask import request
+import json
+app = Flask(__name__)
+
+@app.route('/', methods=['POST'])
+def processing():
+    data = request.data
+    dataDict = json.loads(data)
+    print(dataDict)
+
+    tempResponse = json.dumps(
+        {'woff': 'https://s3.ap-northeast-2.amazonaws.com/fontto/example/LotteMartHappyMedium.woff'}
+    )
+    response = app.response_class(
+        response=tempResponse,
+        status=200,
+        mimetype='application/json'
+    )
+    return response
+
+if __name__ == '__main__':
+    app.run()
