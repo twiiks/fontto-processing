3081abd414e7df457ecc43a57741a593dd379237
sjang42 <jexful92@google.com>
2017-12-10 03:21:31 +0900

    gpu 옵션 변경
    ---
    Redmine Issue Progress / ID / Time :  - - -
    ---
    What :
    - -1 -> 0,1
    
    Why :
    - 디폴트 값 변경
    ---
    *Signed-off-by: sjang42 <jexjang92@gmail.com>*

--- generator/options/base_options.py
+++ generator/options/base_options.py
@@ -20,7 +20,7 @@ class BaseOptions():
         self.parser.add_argument('--which_model_netD', type=str, default='basic', help='selects model to use for netD')
         self.parser.add_argument('--which_model_netG', type=str, default='unet_256', help='selects model to use for netG')
         self.parser.add_argument('--n_layers_D', type=int, default=3, help='only used if which_model_netD==n_layers')
-        self.parser.add_argument('--gpu_ids', type=str, default='-1', help='gpu ids: e.g. 0  0,1,2, 0,2. use -1 for CPU')
+        self.parser.add_argument('--gpu_ids', type=str, default='0,1', help='gpu ids: e.g. 0  0,1,2, 0,2. use -1 for CPU')
         self.parser.add_argument('--name', type=str, default='experiment_name', help='name of the experiment. It decides where to store samples and models')
         self.parser.add_argument('--dataset_mode', type=str, default='single', help='chooses how datasets are loaded. [unaligned | aligned | single]')
         self.parser.add_argument('--model', type=str, default='test',
